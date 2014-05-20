a4cc88f6e9c7d7093a25ac4e30da876af6181f3d
gabrielbaudouin <gabriel.baudouin1@gmail.com>
2014-05-20 17:45:45 +0200

    detectUSB

---
+++ formationCP/DetectUSB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formationCP
+{
+    class DetectUSB : Form1
+    {
+            Form1 form_princ = new Form1();
+        
+        public void DetectionUSB()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    form_princ.champ_fichier.Text += drive.Name;
+                }
+            }
+
+        }
+
+    }
+}
