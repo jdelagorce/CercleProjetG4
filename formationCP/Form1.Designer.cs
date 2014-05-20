b75623d2a9c39acea25076b366bf58e88b69ef85
gabrielbaudouin <gabriel.baudouin1@gmail.com>
2014-05-20 09:55:59 +0200

    Add new project to selecting files

---
+++ formationCP/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace formationCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Parcourir = new System.Windows.Forms.Button();
+            this.champ_fichier = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listFiles = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Parcourir
+            // 
+            this.Parcourir.Location = new System.Drawing.Point(242, 82);
+            this.Parcourir.Name = "Parcourir";
+            this.Parcourir.Size = new System.Drawing.Size(153, 39);
+            this.Parcourir.TabIndex = 0;
+            this.Parcourir.Text = "Parcourir";
+            this.Parcourir.UseVisualStyleBackColor = true;
+            this.Parcourir.Click += new System.EventHandler(this.Parcourir_Click);
+            // 
+            // champ_fichier
+            // 
+            this.champ_fichier.Location = new System.Drawing.Point(242, 152);
+            this.champ_fichier.Multiline = true;
+            this.champ_fichier.Name = "champ_fichier";
+            this.champ_fichier.Size = new System.Drawing.Size(198, 101);
+            this.champ_fichier.TabIndex = 1;
+            // 
+            // listFiles
+            // 
+            this.listFiles.AllowDrop = true;
+            this.listFiles.Location = new System.Drawing.Point(12, 12);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(65, 241);
+            this.listFiles.TabIndex = 2;
+            this.listFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Red;
+            this.progressBar1.Location = new System.Drawing.Point(298, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(142, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 337);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.champ_fichier);
+            this.Controls.Add(this.Parcourir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Parcourir;
+        private System.Windows.Forms.TextBox champ_fichier;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView listFiles;
+        private System.Windows.Forms.ProgressBar progressBar1;
+
+    }
+}
+
---
+++ formationCP/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//include supplémentaire
+using System.Runtime.InteropServices;
+
+
+namespace formationCP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private string Parseur_fichier(string chemin)
+        {
+            string[] fichier = chemin.Split('\\');
+            return fichier[fichier.Length - 1];
+        }
+
+        private void Parcourir_Click(object sender, EventArgs e)
+        {
+            champ_fichier.Clear();
+            listFiles.Clear();
+
+            System.Windows.Forms.OpenFileDialog Parcourir_file = new System.Windows.Forms.OpenFileDialog();
+
+            Parcourir_file.Multiselect = true;
+
+            // Call the ShowDialog method to show the dialog box.
+            DialogResult result = Parcourir_file.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (result == DialogResult.OK)
+            {
+                int i = 0;
+                foreach(string filename in Parcourir_file.FileNames)
+                {
+                    
+                champ_fichier.Text += "'";
+                champ_fichier.Text += filename+ "'" + Environment.NewLine;
+                listFiles.Items.Add(Parseur_fichier(filename), i);
+                }
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.Value = 50;
+            progressBar1.BackColor = Color.Black;
+            progressBar1.ForeColor = Color.Red;
+
+        }
+        
+        //drag and drop
+
+
+
+    }
+    /*public class NewProgressBar : ProgressBar
+    {
+        public NewProgressBar()
+        {
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            // None... Helps control the flicker.
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            const int inset = 2; // A single inset value to control teh sizing of the inner rect.
+
+            using (Image offscreenImage = new Bitmap(this.Width, this.Height))
+            {
+                using (Graphics offscreen = Graphics.FromImage(offscreenImage))
+                {
+                    Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+
+                    if (ProgressBarRenderer.IsSupported)
+                        ProgressBarRenderer.DrawHorizontalBar(offscreen, rect);
+
+                    rect.Inflate(new Size(-inset, -inset)); // Deflate inner rect.
+                    rect.Width = (int)(rect.Width * ((double)this.Value / this.Maximum));
+                    if (rect.Width == 0) rect.Width = 1; // Can't draw rec with width of 0.
+
+                    LinearGradientBrush brush = new LinearGradientBrush(rect, this.BackColor, this.ForeColor, LinearGradientMode.Vertical);
+                    offscreen.FillRectangle(brush, inset, inset, rect.Width, rect.Height);
+
+                    e.Graphics.DrawImage(offscreenImage, 0, 0);
+                    offscreenImage.Dispose();
+                }
+            }
+        }
+    }*/
+    
+}
